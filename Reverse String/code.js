af37d509143ae3707d8cd534c4c6a048d9f86369
mauricechouam <lansichouamou@gmail.com>
2020-10-18 18:15:56 -0700

    added code source

---
+++ Reverse String/code.js
@@ -0,0 +1,8 @@
+var reverseString = function (s) {
+    for (let i = 0; i < Math.floor(s.length / 2); i++) {
+        let temp = s[i];
+        s[i] = s[s.length - 1 - i]
+        s[s.length - 1 - i] = temp;
+    }
+
+};
