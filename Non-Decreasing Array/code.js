2e65720d42566ddbb52c539f69d7cef5734fa2b2
mauricechouam <lansichouamou@gmail.com>
2020-10-20 21:21:36 -0700

    added code source

--- Non-Decreasing Array/code.js
+++ Non-Decreasing Array/code.js
@@ -1,4 +1,14 @@
 var checkPossility = function (nums) {
     var changed = 0;
+    for (let i = 0; i < nums.length - 1; i++) 
+        if (nums[i] > nums[i + 1]) {
+            if (changed) return 0;
+            if (i != 0 && nums[i - 1] > nums[i + 1]) {
+                nums[i + 1] = nums[i];
+            }
+            changed = 1;
+        }
+        return true;
+    };
+
     
-}
