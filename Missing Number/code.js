e73ddaa48588ff29a4fd619b9164b6613feeb51b
mauricechouam <lansichouamou@gmail.com>
2020-10-27 19:13:41 -0700

    aded comment

--- Missing Number/code.js
+++ Missing Number/code.js
@@ -1,5 +1,5 @@
 var missingNumber = function (nums) {
-    let xor = 0; 
+    let xor = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] != 0)// remove thyis condition keeping the body if no zero slot
             xor ^= nums[i];
@@ -7,5 +7,6 @@ var missingNumber = function (nums) {
     
     }
     return xor;
-    
-}
+    //return XOR ^ ARRAY.length + 1; if your array doesn't have empty zero slot. 
+
+};
