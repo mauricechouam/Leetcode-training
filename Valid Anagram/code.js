f6a74e6adefe463720dd4d91965fcc3c934860d3
mauricechouam <lansichouamou@gmail.com>
2020-10-28 21:40:18 -0700

    added Fucntion

---
+++ Valid Anagram/code.js
@@ -0,0 +1,26 @@
+/*
+  @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(a, b) {
+    
+    if(a.length!==b.length) {return false} 
+        
+      y = a.split("").sort();
+      z = b.split("").sort();
+        
+      if(y.length==z.length) {
+          
+       for (i=0; i<y.length; i++) {
+           
+        if (y[i]!==z[i]){
+            
+         return false;
+        }
+       }
+       return true;
+      } 
+            
+        };
+        
