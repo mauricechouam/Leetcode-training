d0f5d4c0486f0a7da93b9a2143b24c15624f6aca
mauricechouam <lansichouamou@gmail.com>
2020-11-05 23:28:34 -0800

    added code source

---
+++ Greatest Number of Candies/code.js
@@ -0,0 +1,14 @@
+/*
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    var s=[];
+    var max=candies[0];
+    for(i=1;i<candies.length;i++){
+        if(candies[i]>max){max =candies[i]}; 
+    }
+    
+    
+};
