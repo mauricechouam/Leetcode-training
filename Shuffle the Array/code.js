98a6781659c694e6530c20c5f7f618105d9354d4
mauricechouam <lansichouamou@gmail.com>
2020-11-05 23:24:53 -0800

    added code source

--- Shuffle the Array/code.js
+++ Shuffle the Array/code.js
@@ -4,11 +4,19 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    var aux = [];
     var t =[];
     t[0]=nums[0];
     var j=0
- 
+    for(let i=0;i<nums.length;i++){
+        
+        if(i%2==0){
+            t[i]=nums[j];
+        }
+        else{
+            t[i]=nums[n+j]
+            j++
+      }     
+    }
   
     return t;
 };
