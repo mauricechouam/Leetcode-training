553d613e5fcaed617a7e0fb7499e76f6139ba753
mauricechouam <lansichouamou@gmail.com>
2020-11-05 23:16:40 -0800

    added code source

--- Search in Rotated Sorted Array/code.js
+++ Search in Rotated Sorted Array/code.js
@@ -16,4 +16,20 @@ var search = function (nums, target) {
         else if (nums[mid] > nums[end]) start = mid + 1;
         else end = mid - 1;
     }
+    if(target < nums[0]) {
+        start = min;
+        end = nums.length-1;
+    } else {
+        start = 0;
+        // if min = 0 set end to last index b/c the array is sorted.
+        end = min || nums.length-1;
+    }
+    
+    while(start <= end) {
+        mid = ~~((start+end)/2);
+        if(nums[mid] === target) return mid;
+        if(nums[mid] > target) end = mid-1;
+        else start = mid+1;
+    }
+    return -1;
 };
