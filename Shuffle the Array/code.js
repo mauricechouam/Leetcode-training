cdbc9a9692c75eca46c597e32314b29628aeb1fa
mauricechouam <lansichouamou@gmail.com>
2020-11-05 23:24:33 -0800

    added code source

---
+++ Shuffle the Array/code.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    var aux = [];
+    var t =[];
+    t[0]=nums[0];
+    var j=0
+ 
+  
+    return t;
+};
