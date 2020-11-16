a746caf81baa6b19f9cbcf2ef1da82d100359468
mauricechouam <lansichouamou@gmail.com>
2020-11-15 22:02:16 -0800

    added function

---
+++ 1-bit and 2-bit Characters/code.js
@@ -0,0 +1,13 @@
+/*
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function(bits) {
+    
+    var  i = 0;
+       while (i < bits.length - 1) {
+           i += bits[i] + 1;
+       }
+       return i == bits.length - 1;
+   };
+   
