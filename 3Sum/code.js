9af8c5533f7b1acb56f4afcc37f0814be992c223
mauricechouam <lansichouamou@gmail.com>
2020-12-15 22:22:30 -0800

    added Description

--- 3Sum/code.js
+++ 3Sum/code.js
@@ -18,7 +18,27 @@ var threeSum = function(nums) {
 
 		let j = i + 1
 
-		let k = nums.length - 1
+        let k = nums.length - 1
+        
+        while (j < k) {
+			let sum = nums[i] + nums[j] + nums[k]
+
+			if (sum === target) {
+				results.push([nums[i], nums[j], nums[k]])
+
+				while (nums[j] === nums[j + 1]) j++
+				while (nums[k] === nums[k - 1]) k--
+
+				j++
+				k--
+
+			} else if (sum < target) {
+				j++
+
+			} else { 
+				k--
+			}
+		}
 		
 
 		
