92570711285dd7f5d73672c5ab79d638bdf57373
mauricechouam <lansichouamou@gmail.com>
2021-01-03 22:23:17 -0800

    Added Function

--- Increasing Triplet Subsequence/code.js
+++ Increasing Triplet Subsequence/code.js
@@ -6,7 +6,15 @@ var increasingTriplet = function (nums) {
     let one = Infinity;
     let two = Infinity;
   
-  
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] <= one) {
+        one = nums[i];
+      } else if (nums[i] <= two) {
+        two = nums[i];
+      } else {
+        return true;
+      }
+    }
   
     return false;
   };
