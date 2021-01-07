0594ef239a5ecc6536173ffd931a8558968e1e8d
mauricechouam <lansichouamou@gmail.com>
2021-01-06 22:39:53 -0800

    added Function

---
+++ Permutation in String/code.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    const map = {};
+    let count = s1.length;
+    
+    for (const c of s1) {
+        if (!map[c]) map[c] = 0;
+        map[c]++;
+    }
+    
+    let l = 0;
+    let i = 0;
+    
+    while (i < s2.length) {
+        const c = s2[i];
+        
+        if (map[c]) {
+            if (count === s1.length) l = i;
+            map[c]--;
+            count--;
+            if (count === 0) return true;
+            i++;
+        } else {
+            if (count === s1.length) {
+                i++;
+            } else {
+                map[s2[l]]++;
+                count++;
+                l++;
+            }
+        }
+    }
+    
+    return false;
+};
