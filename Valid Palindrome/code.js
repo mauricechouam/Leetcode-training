57e8a3096b711e417d59a3f8f354ecd9d42d951b
mauricechouam <lansichouamou@gmail.com>
2021-01-23 23:27:45 -0800

    added function

---
+++ Valid Palindrome/code.js
@@ -0,0 +1,11 @@
+/*
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = s => {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+      if (s.charAt(i) !== s.charAt(j)) return false;
+    }
+    return true;
+  }
