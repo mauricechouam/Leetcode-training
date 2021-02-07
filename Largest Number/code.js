d7eccc62179f8b1e5de5ae99af180d1487eae314
mauricechouam <lansichouamou@gmail.com>
2021-02-07 02:28:12 -0800

    added function

---
+++ Largest Number/code.js
@@ -0,0 +1,15 @@
+/*
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    nums.sort((a, b) => {
+        return (b + '' + a) - (a + '' + b);
+    });
+
+    if(nums[0] === 0) {
+        return '0';
+    }
+
+    return nums.join('')
+};
