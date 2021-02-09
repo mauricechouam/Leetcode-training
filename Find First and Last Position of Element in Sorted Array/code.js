4458de232a0877e80d34e0e10b7a134dec0cecea
mauricechouam <lansichouamou@gmail.com>
2021-02-09 00:25:05 -0800

    added function

--- Find First and Last Position of Element in Sorted Array/code.js
+++ Find First and Last Position of Element in Sorted Array/code.js
@@ -3,6 +3,26 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function (nums, target) {
-    let low = 0, high = nums.length - 1, mid;
-}
+var searchRange = function(nums, target) {
+    let low = 0, high = nums.length-1, mid;
+    
+    // find the start
+    while(low <= high) {
+        mid = Math.floor((low+high)/2);
+        if(nums[mid] >= target) high = mid-1;
+        else low = mid+1;
+    }
+    
+ 
+    
+    // reset low and high
+    low = 0, high = nums.length-1;
+    
+    // find the end
+    while(low <= high) {
+        mid = Math.floor((low+high)/2);
+        if(nums[mid] <= target) low = mid+1;
+        else high = mid-1;
+    }
+    return [start, high];
+};
