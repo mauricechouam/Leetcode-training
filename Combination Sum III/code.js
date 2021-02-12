25cf8465605750ba85ecb870c7283481a204c457
mauricechouam <lansichouamou@gmail.com>
2021-02-11 23:02:13 -0800

    added some function

--- Combination Sum III/code.js
+++ Combination Sum III/code.js
@@ -3,3 +3,19 @@
  * @param {number} n
  * @return {number[][]}
  */
+var combinationSum3 = function(k, n) {
+    let res = [];
+    let temp = [];
+    let target = n;
+
+    const fun = (sum, index) => {
+        if (sum == target) {
+            if(temp.length ==k) res.push([...temp]);
+            return;
+        }
+     
+    }
+
+    
+    
+};
