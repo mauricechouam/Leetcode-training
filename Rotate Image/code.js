9e949564e0e1a93d81ee8bc6aca094111747a772
mauricechouam <lansichouamou@gmail.com>
2021-02-13 19:07:11 -0800

    added Function

--- Rotate Image/code.js
+++ Rotate Image/code.js
@@ -4,7 +4,15 @@
  */
 var rotate = function(matrix) {
  
-   
+    let n = matrix.length 
+    //rows become columns and columns become rows  
+    for(let i = 0; i<n; i++){
+        for(let j = i; j< n; j++){
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix [j][i] = temp
+        }
+    }
    
 
 }; 
