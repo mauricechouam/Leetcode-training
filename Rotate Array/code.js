8e141bb8ffe3f86c62f0a846bcbee6ec328cf369
mauricechouam <lansichouamou@gmail.com>
2021-02-14 23:19:45 -0800

    added Function

--- Rotate Array/code.js
+++ Rotate Array/code.js
@@ -6,12 +6,5 @@
 var rotate = function(nums, k) {
     k %= nums.length;
    var temp, previous;
-   for (let i = 0; i < k; i++) {
-     previous = nums[nums.length - 1];
-     for (let j = 0; j < nums.length; j++) {
-       temp = nums[j];
-       nums[j] = previous;
-       previous = temp;
-     }
-   }
+  
  };
