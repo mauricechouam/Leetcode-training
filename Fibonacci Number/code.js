24b0361e86a2f70600f17a0677e6f86c86629189
mauricechouam <lansichouamou@gmail.com>
2021-02-18 19:04:42 -0800

    added Function

--- Fibonacci Number/code.js
+++ Fibonacci Number/code.js
@@ -4,16 +4,14 @@
  */
 var fib = function(N) {
     if (N === 0 || N === 1) return N;
-    let a = 0;
-    let b = 1;
-    let i = 2;
+   
 
     for (; i <= N; i += 1) { }
-    
+
     
    
     
     
     
     return b;
-  };
+  }; 
