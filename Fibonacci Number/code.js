c74a8419a9d9d2bf24e6d21618c5af5210903577
mauricechouam <lansichouamou@gmail.com>
2021-02-24 21:05:58 -0800

    added New variable

--- Fibonacci Number/code.js
+++ Fibonacci Number/code.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var fib = function(N) {
-    if (N === 0 || N === 1) return N;
+  if (N === 0 || N === 1) return N;
+  let a = 0;
     
     for (; i <= N; i += 1) {
         const temp = a + b;
