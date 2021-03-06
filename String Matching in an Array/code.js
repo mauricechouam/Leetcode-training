10272058e1c0909a48a57b0b2d8af0c8232a782f
mauricechouam <lansichouamou@gmail.com>
2021-03-05 22:17:17 -0800

    added function

--- String Matching in an Array/code.js
+++ String Matching in an Array/code.js
@@ -8,12 +8,10 @@ var stringMatching = function (words) {
     for (let i = 0; i < words.length - 1; i++) { 
         for (let j = i + 1; j < words.length; j++) {
             if (words[j].includes(words[i])) { 
-                
+
             }
-            
 
         }
-
     }
 
     
