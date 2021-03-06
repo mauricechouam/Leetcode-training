f622c5c79c7fee3a864143eaf8d3804f80e85624
mauricechouam <lansichouamou@gmail.com>
2021-03-05 22:16:50 -0800

    added function

--- String Matching in an Array/code.js
+++ String Matching in an Array/code.js
@@ -6,8 +6,12 @@ var stringMatching = function (words) {
     let res = [];
     words.sort((a, b) => a.length - b.length);
     for (let i = 0; i < words.length - 1; i++) { 
-        for (let j = i + 1; j < words.length; j++) { 
+        for (let j = i + 1; j < words.length; j++) {
+            if (words[j].includes(words[i])) { 
+                
+            }
             
+
         }
 
     }
