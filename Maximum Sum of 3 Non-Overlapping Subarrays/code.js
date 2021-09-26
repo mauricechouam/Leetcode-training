98cbf581c19b451bf17392029fb8448b856b3081
mauricechouam <lansichouamou@gmail.com>
2021-09-25 20:30:17 -0700

    added function

--- Maximum Sum of 3 Non-Overlapping Subarrays/code.js
+++ Maximum Sum of 3 Non-Overlapping Subarrays/code.js
@@ -8,10 +8,9 @@ var maxSumofThreesubarrays = function (nums, k, m = 3) {
     let len = nums.length - k + 1;
     const memo = array(len).fill(0);
     memo[0] = nums.slice(k).reduce((acc, n) => acc + n);
-    memo[i] = memo[i - 1] - nums[i - 1] + nums[i + k - 1];
-    
-    for (let i = 1; i < nums.length - k + 1; i++){
-        
+    for (let i = 1; i < nums.length - k + 1; i++) {
+        memo[i] = memo[i - 1] - nums[i - 1] + nums[i + k - 1];
+
     }
 
 }
