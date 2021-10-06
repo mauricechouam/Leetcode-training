b7a051c7cc1c5645ab17356db67a1fd4f1737b29
mauricechouam <lansichouamou@gmail.com>
2021-10-05 22:07:51 -0700

    added New Function

--- Best Time to Buy and Sell Stock III/code.js
+++ Best Time to Buy and Sell Stock III/code.js
@@ -7,7 +7,7 @@ var maxProfit = function (prices) {
     let t2Cost = Infinity;
     for (let i = 0; i < prices.length; i++)
     {
-        t1Cost= Math.min
+        t1Cost = Math.min(t1Cost, prices[i] - t1Cost);
         
     }
 
