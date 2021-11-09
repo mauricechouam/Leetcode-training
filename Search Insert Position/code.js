355f4f5e212ba3e77a77d24c26251a23bed3bfd3
mauricechouam <lansichouamou@gmail.com>
2021-11-08 19:59:41 -0800

    added function

--- Search Insert Position/code.js
+++ Search Insert Position/code.js
@@ -8,18 +8,17 @@ var searchInsert = function (nums, target) {
     let start = 0;
     let end = nums.length - 1;
     while (start <= end) {
-        const middle = Math.floor((end + start)/2);
+        const middle = Math.floor((end + start) / 2);
         if (nums[middle === target]) {
-            return middle;   
-       }
+            return middle;
+        }
         if (nums[middle] < target) {
             start = middle + 1;
             continue;
         }
         if (nums[middle] < target) {
             start = middle
-          }
+        }
     }
-
 
 }
